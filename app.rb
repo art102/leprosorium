dff21ae27c21aa5cdcf02025be50148b56dba15a
ArturDaukaev <daukaevar@bashneft.ru>
2017-08-01 19:56:03 +0500

    Add parameter validation

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ def init_db
 	@db.results_as_hash = true
 end
 
+
+# before вызывается каждый раз при перезагрузке любой страницы
 before do
 	init_db
 end
@@ -34,12 +36,22 @@ get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
+
+# обработчик get запроса для /new
+# (браузер получает страницу с сервера)
 get '/new' do
   erb :new
 end
 
+# обработчик post запроса /new
+# (браузер отправляет данные на сервер)
 post '/new' do
   content = params[:content]
+
+  if content.size <= 0
+  	@error = 'Please enter text'
+  	return erb :new
+  end
 
   erb "You typed: #{content}"
 end
