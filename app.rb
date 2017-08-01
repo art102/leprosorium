2e6da756a023ad21ecb4bd74c8519c6c2ab3a628
ArturDaukaev <daukaevar@bashneft.ru>
2017-08-01 18:46:24 +0500

    Create table posts

--- app.rb
+++ app.rb
@@ -13,6 +13,18 @@ before do
 	init_db
 end
 
+# Этот код вызывается, когда мы обновляем страницу или изменяем конфигурацияю
+configure do
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts
+	(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		crerated_date DATE, 
+		content TEXT
+	)'
+
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
